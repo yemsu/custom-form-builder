f6d9cdbb6504e2be0377cb25b6c2105c0aaf6ef3
정수진 <sssjsjj@gmail.com>
2025-03-10 15:17:19 +0900

    chore: vitest 설치 및 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,28 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	plugins: [
+		tailwindcss(),
+		{
+			...reactRouter(),
+			apply: 'serve'
+		},
+		,
+		tsconfigPaths()
+	],
 	server: {
 		host: '0.0.0.0',
 		port: 3000,
 		watch: {
 			usePolling: true
 		}
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['./vitest.setup.ts']
 	}
 })
